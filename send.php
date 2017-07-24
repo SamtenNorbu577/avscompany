7dc5195acc96bb9d329f193b2eb9f0d6cc5ef40e
kain577 <mahakala577@gmail.com>
2017-07-24 22:35:38 +0500

    fix email

--- send.php
+++ send.php
@@ -3,4 +3,4 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 
-mail("admin@avscompany.ru", "Заявка с сайта", "ФИО: ".$name.". Телефон: ".$phone.". E-mail: ".$email, "From: site@avscompany.ru \r\n");
+mail("mahakala577@gmail.com", "Заявка с сайта", "ФИО: ".$name.". Телефон: ".$phone.". E-mail: ".$email, "From: site@avscompany.ru \r\n");
