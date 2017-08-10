c373a08e1e3d86ff3908ddac6f94a652c162304d
Kain577 <mahakala577@gmail.com>
2017-08-10 23:17:32 +0500

    made a lot of fix

--- scripts/panorama.js
+++ scripts/panorama.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 		});
 	var counter = 2;
 	function sec() {
+		var dots = document.getElementsByClassName('js-nav-dot');
 		var back = document.getElementById('back');
 		var img1 = "url('image/banner/cont-1.jpg')";
 		var img2 = "url('image/banner/cont-2.jpg')"; 
@@ -15,14 +16,20 @@ $(document).ready(function(){
 		if(counter == 1){
 			back.style.backgroundImage = img1;
 			counter = 2;
+			dots[0].classList.add("l-nav__dot-active");
+			dots[2].classList.remove("l-nav__dot-active");
 		}
 		else if(counter == 2){
 			back.style.backgroundImage = img2;
 			counter = 3;
+			dots[1].classList.add("l-nav__dot-active");
+			dots[0].classList.remove("l-nav__dot-active");
 		}
 		else{
 			back.style.backgroundImage = img3;
 			counter = 1;
+			dots[2].classList.add("l-nav__dot-active");
+			dots[1].classList.remove("l-nav__dot-active");
 		}
 	}
 	setInterval(sec, 5000);
